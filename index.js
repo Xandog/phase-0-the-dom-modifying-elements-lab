be93171f28f91c0e18b1533802449786f6d61631
Alex Aguirre <aguirre_alex@rocketmail.com>
2021-08-23 02:12:40 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+main.remove(document.getElementById('main'));
+
+const newHeader = document.createElement('h1');
+
+document.body.appendChild(newHeader);
+
+newHeader.id = 'victory';
+
+newHeader.textContent = "Alex is the champion";
